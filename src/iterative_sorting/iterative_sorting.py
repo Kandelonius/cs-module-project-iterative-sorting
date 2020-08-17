d01741b18472a65305b8c25097e7971d8ccb19cf
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-17 16:56:04 -0700

    have both tests passing now.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,11 +1,10 @@
-# TO-DO: Complete the selection_sort() function below
 import random
+
 
 def selection_sort(arr):
     # loop through n-1 elements
     for outer in range(0, len(arr) - 1):
         cur_index = outer  # set iterating index
-        # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         ## set the first index as minimum
         smallest_index = cur_index  # set index of the smallest element
@@ -13,13 +12,13 @@ def selection_sort(arr):
         for inner in range(outer + 1, len(arr)):  # loop through the array starting with the current index
             if arr[inner] < arr[
                 smallest_index]:  # check if the item at the inner arrays current is smaller than the previous smallest
-                smallest_index = inner # set smallest index to the new smaller index
+                smallest_index = inner  # set smallest index to the new smaller index
             ## then continue to the end of the array checking each element.
             ## if none are smaller, swap smallest with current then continue until all elements are sorted.
 
         # TO-DO: swap
-        temp = arr[outer] # store leftmost value
-        arr[outer] = arr[smallest_index] # set leftmost value to smallest value
+        temp = arr[outer]  # store leftmost value
+        arr[outer] = arr[smallest_index]  # set leftmost value to smallest value
         arr[smallest_index] = temp
 
         # (arr[outer], arr[smallest_index]) = (
@@ -28,10 +27,20 @@ def selection_sort(arr):
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    """
+    start at the first index and compare the first and second items
+    if the second item is smaller, swap them and evaluate second and third the same way
+    """
+    for outer in range(len(arr)): # times looping through the full array
+        for inner in range(0, len(arr) - outer - 1): # loop through the remaining elements
+            # checking if left is larger than right
+            if arr[inner] > arr[inner + 1]:
+                # then swapping if it is
+                temp = arr[inner + 1] # storing the value of the earlier element
+                arr[inner + 1] = arr[inner]
+                arr[inner] = temp
+    print(arr)
     return arr
 
 
@@ -60,8 +69,10 @@ def counting_sort(arr, maximum=None):
     return arr
 
 
-# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 # (selection_sort(arr1))
-arr4 = random.sample(range(200), 50)
 
-(selection_sort(arr4), sorted(arr4))
+bubble_sort(arr1)
+# arr4 = random.sample(range(200), 50)
+#
+# (selection_sort(arr4), sorted(arr4))
