006a4606a239ea86d2438164cb10ff55e492ccd0
Shane Kaestner <shane.kaestner@yahoo.com>
2020-08-17 15:32:16 -0700

    got the test for selection sort working.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,30 @@
 # TO-DO: Complete the selection_sort() function below
+import random
+
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+    for outer in range(0, len(arr) - 1):
+        cur_index = outer  # set iterating index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
+        ## set the first index as minimum
+        smallest_index = cur_index  # set index of the smallest element
+        ## compare minimum with the next element and if that is smaller reset minimum
+        for inner in range(outer + 1, len(arr)):  # loop through the array starting with the current index
+            if arr[inner] < arr[
+                smallest_index]:  # check if the item at the inner arrays current is smaller than the previous smallest
+                smallest_index = inner # set smallest index to the new smaller index
+            ## then continue to the end of the array checking each element.
+            ## if none are smaller, swap smallest with current then continue until all elements are sorted.
 
         # TO-DO: swap
-        # Your code here
+        temp = arr[outer] # store leftmost value
+        arr[outer] = arr[smallest_index] # set leftmost value to smallest value
+        arr[smallest_index] = temp
 
+        # (arr[outer], arr[smallest_index]) = (
+        #     arr[cur_index], arr[outer])  # using stored indexes and values to make the swap
+    # print(arr)
     return arr
 
 
@@ -19,8 +32,8 @@ def selection_sort(arr):
 def bubble_sort(arr):
     # Your code here
 
-
     return arr
+
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -39,8 +52,16 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
+
+
 def counting_sort(arr, maximum=None):
     # Your code here
 
-
     return arr
+
+
+# arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# (selection_sort(arr1))
+arr4 = random.sample(range(200), 50)
+
+(selection_sort(arr4), sorted(arr4))
